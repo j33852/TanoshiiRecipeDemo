5f234bdf0f78e79a54a1872f642a28a589e87e5b
j33852 <j33852@gmail.com>
2023-07-17 13:49:23 +0900

    Initial Commit

---
+++ TanoshiiRecipeDemo/TanoshiiRecipeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TanoshiiRecipeDemoApp.swift
+//  TanoshiiRecipeDemo
+//
+//  Created by 張俊傑 on 2023/07/17.
+//
+
+import SwiftUI
+
+@main
+struct TanoshiiRecipeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TanoshiiRecipeDemoUITests/TanoshiiRecipeDemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TanoshiiRecipeDemoUITestsLaunchTests.swift
+//  TanoshiiRecipeDemoUITests
+//
+//  Created by 張俊傑 on 2023/07/17.
+//
+
+import XCTest
+
+final class TanoshiiRecipeDemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
